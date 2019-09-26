e498dde1d29db7a5e8adcd104bb347702d090d8a
Zalan Blenessy <zalan@radixdlt.com>
2019-09-26 11:31:12 +0200

    Using Thread.@spawn in julia 1.3 and optimal task reduction algo.

--- src/Threads.jl
+++ src/Threads.jl
@@ -1,41 +1,22 @@
-function reduceone(func::Function, args)
-    # split arguments into batches with an even number
-    n = length(args)
-    numreductions = div(n, 2)
-    reductions = Vector{eltype(args)}(undef, numreductions + n & 1)
-    Threads.@threads for i in 1:numreductions
-        j = i << 1
-        reductions[i] = func(args[j-1], args[j])
+"""
+This function reduces the input tasks to a single task by applying `func` to each input task pair.
+"""
+function reducetasks(func, tasks)
+    @assert !isempty(tasks)
+    todo = collect(tasks)
+    while length(todo) > 1
+        a, b = popfirst!(todo), popfirst!(todo)
+        newtask = Threads.@spawn func(fetch(a), fetch(b))
+        push!(todo, newtask)
     end
-    if isodd(n)
-        reductions[end] = args[end]
-    end
-    return reductions
-end
-
-function reduceall(func::Function, args)
-    # split arguments into batches with an even number
-    reductions = Vector{eltype(args)}()
-    for batchargs in Iterators.partition(args, Config.BATCH_SIZE)
-        append!(reductions, reduceone(func, batchargs))
-        if length(reductions) > Config.BATCH_SIZE
-            reductions = reduceone(func, reductions)
-        end
-    end
-    while length(reductions) > 1
-        reductions = reduceone(func, reductions)
-    end
-    return reductions[1]
+    return todo[1]
 end
 
 function verify(::Type{SpawnThreads}, sig::Model.Signature, pkhashpairs)
-    # NOTE: This is PoC only! It demonstrates that the feasibility/benefits of running thread level parallelism with RelicToolkit 
-    lhside_results = Vector(undef, length(pkhashpairs)+1)
-    Threads.@threads for i in 0:length(pkhashpairs)
-        a, b = iszero(i) ? (Config.G2, sig.sig) : (pkhashpairs[i].first.pk, pkhashpairs[i].second.hash)
-        lhside_results[i+1] = Curve.curve_miller(a, b)
-    end
-    rside = popfirst!(lhside_results)
-    lhside = reduceall(*, lhside_results)
-    return isone(Curve.field_final_exp(lhside // rside))
+    # kick off workers in parallel
+    task_rhside = Threads.@spawn Curve.curve_miller(Config.G2, sig.sig)
+    tasks_lhside = [Threads.@spawn(Curve.curve_miller(pair.first.pk, pair.second.hash)) for pair in pkhashpairs]
+    task_lhside = reducetasks(*, tasks_lhside)
+    res = Curve.field_final_exp(fetch(task_lhside) // fetch(task_rhside))
+    return res == one(typeof(res))
 end
